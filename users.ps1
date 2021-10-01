75c10094c835e7535857433cda67c1511c6348b7
evanjd711 <40526586+evanjd711@users.noreply.github.com>
2021-10-01 14:16:26 -0700

    initial PS setup and users script

---
+++ users.ps1
@@ -0,0 +1,4 @@
+﻿[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+wget https://github.com/PowerShell/PowerShell/releases/download/v7.1.4/PowerShell-7.1.4-win-x86.msi -OutFile env:\Downloads\ps.msi
+cd Downloads
+msiexec.exe /Package ps.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=0 REGISTER_MANIFEST=1
