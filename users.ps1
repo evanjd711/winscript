3fab557267a24a78fe412cee9b0c3284aa73a666
Evan Deters <evanjd711@gmail.com>
2021-10-01 15:15:47 -0700

    Added local and domain user passwords

--- users.ps1
+++ users.ps1
@@ -5,5 +5,11 @@ msiexec.exe /Package ps.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 EN
 & 'C:\Program Files (x86)\Powershell\7\pwsh.exe'
 $osInfo = Get-CimInstance -ClassName Win32_OperatingSystem
 if($osInfo.ProductType -eq 2) {
-    
+    import-module ActiveDirectory
+    $pass = ConvertTo-SecureString (Read-Host -prompt "Enter the password you will use") -Force
+    Get-ADUser -Filter * | Set-ADAccountPassword -NewPassword $pass -Reset
+} elseif ($osInfo.ProductType -eq 1 -or $osInfo.ProductType -eq 3) {
+    $pass = ConvertTo-SecureString (Read-Host -prompt "Enter the password you will use") -Force
+    Get-LocalUser | Set-LocalUser -Password $pass -UserMayChangePassword $false 
+    Get-LocalUser | Disable-LocalUser
 }
