93d06932dfa834171d81b162fb9bcaf90393f6b2
Evan Deters <evanjd711@gmail.com>
2021-10-02 00:28:23 -0700

    yeet

--- main.ps1
+++ main.ps1
@@ -1,4 +1,5 @@
 Set-ExecutionPolicy RemoteSigned -Scope Process
 Unblock-File -Path $env:USERPROFILE\Downloads\winscript-main\setup.ps1 
-.\setup.ps1
+.\$env:USERPROFILE\Downloads\winscript-main\setup.ps1
 Unblock-File -Path $env:USERPROFILE\Downloads\winscript-main\users.ps1
+.\$env:USERPROFILE\Downloads\winscript-main\users.ps1
--- users.ps1
+++ users.ps1
@@ -1,7 +1,7 @@
 ﻿$osInfo = Get-CimInstance -ClassName Win32_OperatingSystem
 if($osInfo.ProductType -eq 2) {
     import-module ActiveDirectory
-    $pass = ConvertTo-SecureString (Read-Host -prompt "Enter the password you will use") -Force
+    $pass = ConvertTo-SecureString (Read-Host -prompt "Enter the password you will use") -AsPlainText -Force
     Get-ADUser -Filter * | Set-ADAccountPassword -NewPassword $pass -Reset
 } elseif ($osInfo.ProductType -eq 1 -or $osInfo.ProductType -eq 3) {
     $pass = ConvertTo-SecureString (Read-Host -prompt "Enter the password you will use") -Force
