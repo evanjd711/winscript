f39ebb8c3c7c15d7b9b45079777829a8e6219f7f
Evan Deters <evanjd711@gmail.com>
2021-10-01 23:51:22 -0700

    fixed filepaths

--- main.ps1
+++ main.ps1
@@ -1,4 +1,4 @@
 Set-ExecutionPolicy RemoteSigned -Scope Process
-Unblock-File -Path $env:USERPROFILE\Downloads\setup.ps1 
+Unblock-File -Path $env:USERPROFILE\Downloads\winscript-main\setup.ps1 
 .\setup.ps1
-Unblock-File -Path $env:USERPROFILE\Downloads\users.ps1
+Unblock-File -Path $env:USERPROFILE\Downloads\winscript-main\users.ps1
--- setup.ps1
+++ setup.ps1
@@ -1,5 +1,5 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -uri https://github.com/PowerShell/PowerShell/releases/download/v7.1.4/PowerShell-7.1.4-win-x86.msi -OutFile .\Downloads\ps.msi
-cd Downloads
+Invoke-WebRequest -uri https://github.com/PowerShell/PowerShell/releases/download/v7.1.4/PowerShell-7.1.4-win-x86.msi -OutFile $env:USERPROFILE\Downloads\ps.msi
+cd $env:USERPROFILE\Downloads
 msiexec.exe /Package ps.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=0 REGISTER_MANIFEST=1
 & 'C:\Program Files (x86)\Powershell\7\pwsh.exe'
