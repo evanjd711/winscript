f22e0b4a489cde688ebfb9f2e1924cb3c4bcd3b5
Evan Deters <evanjd711@gmail.com>
2021-10-02 10:02:11 -0700

    Changed AD User

--- users.ps1
+++ users.ps1
@@ -3,6 +3,7 @@ if($osInfo.ProductType -eq 2) {
     import-module ActiveDirectory
     $pass = ConvertTo-SecureString (Read-Host -prompt "Enter the password you will use") -AsPlainText -Force
     Get-ADUser -Filter * | Set-ADAccountPassword -NewPassword $pass -Reset
+    Get-ADUser -Filter * | Set-ADUser -AllowReversiblePasswordEncryption $false -CannotChangePassword $false -ChangePasswordAtLogon $false -PasswordNotRequired $false
 } elseif ($osInfo.ProductType -eq 1 -or $osInfo.ProductType -eq 3) {
     $pass = ConvertTo-SecureString (Read-Host -prompt "Enter the password you will use") -Force
     Get-LocalUser | Set-LocalUser -Password $pass -UserMayChangePassword $false 
