e5bb20a0bedf2c66c1d66c15bbcd3a7e4ceb98a1
Evan Deters <evanjd711@gmail.com>
2021-10-03 23:12:06 -0700

    fix

--- main.ps1
+++ main.ps1
@@ -1,7 +1,7 @@
 Set-ExecutionPolicy RemoteSigned -Scope Process
 Unblock-File -Path $env:USERPROFILE\Downloads\winscript-main\setup.ps1 
-.\"$env:USERPROFILE\Downloads\winscript-main\setup.ps1"
+.\winscript-main\setup.ps1
 Remove-Item -Path $env:USERPROFILE\Downloads\winscript-main\setup.ps1
 Unblock-File -Path $env:USERPROFILE\Downloads\winscript-main\users.ps1
-.\"$env:USERPROFILE\Downloads\winscript-main\users.ps1"
+.\winscript-main\users.ps1
 Remove-Item -Path $env:USERPROFILE\Downloads\winscript-main\users.ps1
