4cd36fb95cacff492fdae1ed3fd1086f4249b7b5
Evan Deters <evanjd711@gmail.com>
2021-10-03 23:08:06 -0700

    fix

--- initial.ps1
+++ initial.ps1
@@ -2,7 +2,6 @@ Set-ExecutionPolicy Restricted
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -uri https://github.com/evanjd711/winscript/archive/refs/heads/main.zip -OutFile $env:USERPROFILE\Downloads\win.zip
 cd $env:USERPROFILE\Downloads
-Write-Out "temsep"
 Add-Type -assembly "system.io.compression.filesystem"
 [io.compression.zipfile]::ExtractToDirectory("$env:USERPROFILE\Downloads\win.zip", "$env:USERPROFILE\Downloads")
 Set-ExecutionPolicy RemoteSigned -Scope Process
