1f8e441797ba0038d4a66c7ce275398d7a26d531
Evan Deters <evanjd711@gmail.com>
2021-10-03 22:57:41 -0700

    sfrgsgsrg

--- initial.ps1
+++ initial.ps1
@@ -2,9 +2,9 @@ Set-ExecutionPolicy Restricted
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -uri https://github.com/evanjd711/winscript/archive/refs/heads/main.zip -OutFile $env:USERPROFILE\Downloads\win.zip
 cd $env:USERPROFILE\Downloads
+Write-Out "temp"
 Add-Type -assembly "system.io.compression.filesystem"
 [io.compression.zipfile]::ExtractToDirectory("$env:USERPROFILE\Downloads\win.zip", "$env:USERPROFILE\Downloads")
 Set-ExecutionPolicy RemoteSigned -Scope Process
 Unblock-File .\winscript-main\main.ps1
-Write-Out "temp"
 .\winscript-main\main.ps1
