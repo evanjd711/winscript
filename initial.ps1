2617db13b77c498aa9c5b6a4f7269d7ed8195af3
Evan Deters <evanjd711@gmail.com>
2021-10-03 22:52:22 -0700

    fixed quotes

--- initial.ps1
+++ initial.ps1
@@ -2,7 +2,7 @@ Set-ExecutionPolicy Restricted
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -uri https://github.com/evanjd711/winscript/archive/refs/heads/main.zip -OutFile $env:USERPROFILE\Downloads\win.zip
 cd $env:USERPROFILE\Downloads
-Add-Type -assembly “system.io.compression.filesystem”
+Add-Type -assembly "system.io.compression.filesystem"
 [io.compression.zipfile]::ExtractToDirectory("$env:USERPROFILE\Downloads\win.zip", "$env:USERPROFILE\Downloads")
 Set-ExecutionPolicy RemoteSigned -Scope Process
 Unblock-File .\winscript-main\main.ps1
