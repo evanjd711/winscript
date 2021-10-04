d2c48497181b928685673b9f3f913967cbb29249
Evan Deters <evanjd711@gmail.com>
2021-10-03 23:20:49 -0700

    Switched to BitsTransfer

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,6 @@
 Write-Host "Installing PowerShell 7"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-Invoke-WebRequest -uri https://github.com/PowerShell/PowerShell/releases/download/v7.1.4/PowerShell-7.1.4-win-x86.msi -OutFile $env:USERPROFILE\Downloads\ps.msi
+Start-BitsTransfer -Source https://github.com/PowerShell/PowerShell/releases/download/v7.1.4/PowerShell-7.1.4-win-x86.msi -Destination $env:USERPROFILE\Downloads\ps.msi
 cd $env:USERPROFILE\Downloads
 try {
     msiexec.exe /Package ps.msi /quiet ADD_EXPLORER_CONTEXT_MENU_OPENPOWERSHELL=1 ENABLE_PSREMOTING=0 REGISTER_MANIFEST=1
