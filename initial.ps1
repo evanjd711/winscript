62c385f08d4434c68ddd520a6ebbde43f09105c9
Evan Deters <evanjd711@gmail.com>
2021-10-03 22:47:53 -0700

    fixed initial for Powershell 2013

--- initial.ps1
+++ initial.ps1
@@ -2,8 +2,8 @@ Set-ExecutionPolicy Restricted
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest -uri https://github.com/evanjd711/winscript/archive/refs/heads/main.zip -OutFile $env:USERPROFILE\Downloads\win.zip
 cd $env:USERPROFILE\Downloads
-Expand-Archive win.zip
-copy .\win\winscript-main .\ -r
+Add-Type -assembly “system.io.compression.filesystem”
+[io.compression.zipfile]::ExtractToDirectory("$env:USERPROFILE\Downloads\win.zip", "$env:USERPROFILE\Downloads")
 Set-ExecutionPolicy RemoteSigned -Scope Process
 Unblock-File .\winscript-main\main.ps1
 .\winscript-main\main.ps1
